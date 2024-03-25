25afdd9ea3d289054520f6a2a267a02db8e2c77a
adamr04 <adam.radwan@akg-wien.at>
2024-03-25 13:23:36 +0100

    Revert "ballregeln 2024"
    This reverts commit d0ea4c6a776fb86b00dd5ed6f12bad76eef3d727.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ const Home: React.FC<PageProps> = ({ data, location }) => {
 
   return (
     <Layout location={location} title={siteTitle}>
-      <Seo title="45. Schulball Akademisches Gymnasium Wien" />
+      <Seo title="Der 45. Schulball des Akademischen Gymnasiums Wien" />
       <Hero
         title="OSCARNACHT"
         date="17. Mai 2024"
